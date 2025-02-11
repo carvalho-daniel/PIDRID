1687b3a3aaba9ddeebcc87b0263b810e80768257
Daniel.java <daniel.scarvalho05@gmail.com>
2025-02-11 10:54:28 -0300

    somatorio de horas, mudança do script, modelo do docente

--- js/script.js
+++ js/script.js
@@ -1,34 +1,36 @@
-(function($) {  
-    AddTableRow = function(btn) {
-        var newRow = $("<tr>");
-        var cols = "";
-        
-        // Adicionando as células para a nova linha (Disciplina e Carga Horária)
-        cols += '<td><input type="text" placeholder="Disciplina" class="form-control" /></td>';
-        cols += '<td><input type="number" placeholder="Carga Horária" class="form-control" /></td>';
-        
-        // Colocando o botão de remover na última coluna
-        cols += '<td>';
-        cols += '<button onclick="RemoveTableRow(this)" type="button" class="btn btn-danger btn-sm">-</button>';
-        cols += '</td>';
-        
-        $(newRow).append(cols);
-        
-        // Inserindo a nova linha na tabela
-        $(btn).closest('table').find('tbody').append(newRow);
-        return false;
-    };
-})(jQuery);
+total = 0
 
-(function($) {    
-    RemoveTableRow = function(item) {       
-        var tr = $(item).closest('tr'); 
+function AddTableRow() {
+    // Cria uma nova linha
+    var newRow = $("<tr>");
+    
+    // Adiciona as colunas para a disciplina e carga horária
+    var cols = "";
+    cols += '<td><input type="text" placeholder="Disciplina" class="form-control" /></td>';
+    cols += '<td><input type="number" min=0 placeholder="Carga Horária" class="form-control cargaHorariaDisciplina" oninput="chTotalDisciplina()" /> </td>';
+    cols += '<td style="text-align: center;"><button onclick="RemoveTableRow(this)" type="button" class="btn btn-danger btn-sm">-</button></td>';
+    
+    newRow.append(cols);
 
-        // Removendo a linha após animação
-        tr.fadeOut(400, function() {          
-            tr.remove();        
-        });   
+    // Insere a nova linha acima do botão "Adicione uma nova Disciplina"
+    $(newRow).insertBefore($(event.target).closest('tr'));
 
-        return false;     
-    };   
-})(jQuery);
+}
+
+// Função para remover a linha
+function RemoveTableRow(btn) {
+    var row = $(btn).closest('tr');
+    row.fadeOut(400, function() {
+        row.remove();
+        chTotalDisciplina()
+    });
+}
+
+function chTotalDisciplina() {
+    let total = 0;
+    $(".cargaHorariaDisciplina").each(function() {
+        let valor = parseInt($(this).val()) || 0;
+        total += valor;
+    });
+    $("#totalChDisciplinas").text(total);
+}
---
+++ model/docente.js
@@ -0,0 +1,68 @@
+class docente {
+    nome;
+    departamento = "ENSINO";
+    telefone;
+    email;
+    siape;
+    efetivo=false;
+
+    constructor(nome, telefone, email, siape, efeitvo=false,departamento="ENSINO") {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.siape = siape;
+        this.efetivo = efeitvo;
+        this.departamento = departamento;
+    }
+
+
+    
+    get nome() {
+        return this._nome;
+    }
+    
+    set nome(nome) {
+        this._nome = nome;
+    }
+
+    get departamento() {
+        return this._departamento;
+    }
+    
+    set departamento(departamento) {
+        this._departamento = departamento;
+    }
+
+    get telefone() {
+        return this._telefone;
+    }
+    
+    set telefone(telefone) {
+        this._telefone = telefone;
+    }
+
+    get email() {
+        return this._email;
+    }
+    
+    set email(email) {
+        this._email = email;
+    }
+
+    get siape() {
+        return this._siape;
+    }
+    
+    set siape(siape) {
+        this._siape = siape;
+    }
+
+    get efetivo() {
+        return this._efetivo;
+    }
+    
+    set efetivo(efetivo) {
+        this._efetivo = efetivo;
+    }
+
+}
