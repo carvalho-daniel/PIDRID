bff6cbd6297f60c8a37b7a5e99e778b242d9f7e2
Daniel.java <daniel.scarvalho05@gmail.com>
2025-02-18 11:53:29 -0300

    valor ch
    teste de valores da carga horaria

--- js/preenchePid.js
+++ js/preenchePid.js
@@ -1,4 +1,4 @@
-import { pid } from "../model/pid.js";
+import pid from "../model/pid.js";
 
 function preenchePid() {
     var totalChPrepManu = parseFloat(document.getElementById('totalChPrepManu').innerHTML)
@@ -9,4 +9,30 @@ function preenchePid() {
         alert(c.value)
     }
 
+    // pid.setChPrepManuEnsino();
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Captura o envio do formulário
+    document.getElementById("formPid").addEventListener("submit", function (event) {
+        event.preventDefault(); // Impede o envio padrão do formulário
+
+        // Captura os dados do formulário
+        let formData = new FormData(this);
+        let dadosRecebidos = {};
+
+        // Converte FormData para objeto JSON
+        formData.forEach((value, key) => {
+            dadosRecebidos[key] = value;
+        });
+
+        // Exibe os dados no console (ou pode ser usado de outra forma)
+        console.log("Dados recebidos:", dadosRecebidos);
+        teste();
+    });
+});
+
+function teste() {
+    pid.setChPrepManuEnsino();
 }
