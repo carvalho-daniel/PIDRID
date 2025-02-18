146044aced872296f994388c5c3859cfa3ab1c7e
PMoraizz <pedroamc720@gmail.com>
2025-02-18 11:54:44 -0300

    ciracao rid total
    ciracao rid total

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
 total = 0
 
-function AddTableRow() {
+function AddTableRowPid() {
     // Cria uma nova linha
     var newRow = $("<tr>");
     
     // Adiciona as colunas para a disciplina e carga horária
     var cols = "";
     cols += '<td><input type="text" placeholder="Disciplina" class="form-control" /></td>';
+    cols += '<td><input type="number" min=0 placeholder="Carga Horária" class="form-control cargaHorariaDisciplina" oninput="chTotalDisciplina()" /> </td>';
+    cols += '<td style="text-align: center;"><button onclick="RemoveTableRow(this)" type="button" class="btn btn-danger btn-sm">-</button></td>';
+    
+    newRow.append(cols);
+
+    // Insere a nova linha acima do botão "Adicione uma nova Disciplina"
+    $(newRow).insertBefore($(event.target).closest('tr'));
+
+}
+
+function AddTableRowRid() {
+    // Cria uma nova linha
+    var newRow = $("<tr>");
+    
+    // Adiciona as colunas para a disciplina e carga horária
+    var cols = "";
+    cols += '<td><input type="text" placeholder="Disciplina" class="form-control" /></td>';
+    cols += '<td><p>0</p></td>';
     cols += '<td><input type="number" min=0 placeholder="Carga Horária" class="form-control cargaHorariaDisciplina" oninput="chTotalDisciplina()" /> </td>';
     cols += '<td style="text-align: center;"><button onclick="RemoveTableRow(this)" type="button" class="btn btn-danger btn-sm">-</button></td>';
     
