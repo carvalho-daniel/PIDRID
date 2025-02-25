aaa042a480a19aeed2f7968ddd68d3a6acf676af
Daniel.java <daniel.scarvalho05@gmail.com>
2025-02-25 11:33:18 -0300

    teste

--- js/preenchePid.js
+++ js/preenchePid.js
@@ -1,38 +1,46 @@
 import pid from "../model/pid.js";
 
-function preenchePid() {
-    var totalChPrepManu = parseFloat(document.getElementById('totalChPrepManu').innerHTML)
-    
-    ch = document.querySelectorAll('.prep_manu_ensino');
-
-    for(c of ch) {
-        alert(c.value)
-    }
-
-    // pid.setChPrepManuEnsino();
-
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-    // Captura o envio do formulário
     document.getElementById("formPid").addEventListener("submit", function (event) {
-        event.preventDefault(); // Impede o envio padrão do formulário
-
-        // Captura os dados do formulário
+        event.preventDefault();
+        
         let formData = new FormData(this);
         let dadosRecebidos = {};
 
-        // Converte FormData para objeto JSON
         formData.forEach((value, key) => {
             dadosRecebidos[key] = value;
         });
 
-        // Exibe os dados no console (ou pode ser usado de outra forma)
         console.log("Dados recebidos:", dadosRecebidos);
-        teste();
+        
+        atribuirDadosAoModelo(dadosRecebidos);
     });
 });
 
-function teste() {
-    pid.setChPrepManuEnsino();
+function atribuirDadosAoModelo(dados) {
+    let modeloPid = new pid();
+    
+    for (const key in dados) {
+        let valor = parseFloat(dados[key]) || 0;
+        
+        if (modeloPid.prep_manu_ensino.hasOwnProperty(key)) {
+            modeloPid.prep_manu_ensino[key] = valor;
+        } else if (modeloPid.apoio_ensino.hasOwnProperty(key)) {
+            modeloPid.apoio_ensino[key] = valor;
+        } else if (modeloPid.orientacao.hasOwnProperty(key)) {
+            modeloPid.orientacao[key] = valor;
+        } else if (modeloPid.pesquisa_ino.hasOwnProperty(key)) {
+            modeloPid.pesquisa_ino[key] = valor;
+        } else if (modeloPid.extensao.hasOwnProperty(key)) {
+            modeloPid.extensao[key] = valor;
+        } else if (modeloPid.gestao_inst_repre.hasOwnProperty(key)) {
+            modeloPid.gestao_inst_repre[key] = valor;
+        } else if (modeloPid.quali_capac.hasOwnProperty(key)) {
+            modeloPid.quali_capac[key] = valor;
+        } else if (key === "justificativa") {
+            modeloPid.justificativa = dados[key];
+        }
+    }
+    
+    console.log("Modelo atualizado:", modeloPid);
 }
